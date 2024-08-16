4efd36b0e8e0a96ef64216846690fa412dd94525
Ilya Bylich <ibylich@gmail.com>
2024-08-16 16:55:12 +0200

    add custom app launcher

--- ags/.config/ags/config.js
+++ ags/.config/ags/config.js
@@ -1,6 +1,8 @@
 /// <reference path="/usr/share/com.github.Aylur.ags/types/ags.d.ts" />
 
 import Topbar from "./topbar/index.js"
+import NetworkWidget from "./widgets/network.js"
+import Launcher from "./widgets/launcher.js"
 
 import hyperland from "./services/hyperland.js"
 hyperland.minWorkspaces = 5
@@ -8,10 +10,12 @@ hyperland.minWorkspaces = 5
 import cpu from "./services/cpu.js"
 cpu.refreshIntervalInMs = 1000
 
-
 App.config({
     style: "./style.css",
     windows: [
         Topbar(),
+
+        NetworkWidget,
+        Launcher,
     ],
 })
