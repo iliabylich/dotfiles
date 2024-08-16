cf2ad209877535cdfd9525b48b6170be7da6eeeb
Ilya Bylich <ibylich@gmail.com>
2024-08-16 13:25:25 +0200

    open gnome-system-terminal when clicking on memory

--- ags/.config/ags/topbar/memory.js
+++ ags/.config/ags/topbar/memory.js
@@ -3,9 +3,12 @@
 import memory from "../services/memory.js"
 
 export default function Memory() {
-    return Widget.CenterBox({
-        class_name: "widget memory padded",
-        center_widget: Widget.Label({
+    return Widget.Button({
+        class_name: "widget memory padded clickable",
+        on_primary_click: () => {
+            Utils.execAsync("gnome-system-monitor")
+        },
+        child: Widget.Label({
             label: Utils.merge(
                 [memory.bind("used"), memory.bind("total")],
                 (used, total) => `RAM ${used.toFixed(1)}G/${total.toFixed(1)}G`
